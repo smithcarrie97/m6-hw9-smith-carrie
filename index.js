2027fea4c3b8a75c2e1f5eee00b750306ba41c6a
Carrie Smith <smithcarrierenee@gmail.com>
2021-10-30 15:51:57 -0400

    updated temperatures

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ console.log (formEl, inputEl, weatherEl)
 // fetch weather data if there is a query
 // call render weather function
 formEl.onsubmit = function(e) {
-    e.preventDefault()
-    var weatherQuery = inputEl.value.trim()
-    if (!weatherQuery) return
-    fetch('https://api.openweathermap.org/data/2.5/weather?appid=9e38de1e0875f34d69ce021be2375874&q=' + weatherQuery)
-    .then(function(res) {
-      return res.json()
-    })
-    .then(function(res) {
-      console.log(res)
-      renderWeather(res)
-      inputEl.value = ""
-    })
-    .catch(function(err) {
-      console.log(err)
-    })
+  e.preventDefault()
+  var weatherQuery = inputEl.value.trim()
+  if (!weatherQuery) return
+  fetch('https://api.openweathermap.org/data/2.5/weather?appid=9e38de1e0875f34d69ce021be2375874&units=imperial&q=' + weatherQuery)
+  .then(function(res) {
+    return res.json()
+  })
+  .then(function(res) {
+    console.log(res)
+    renderWeather(res)
+    inputEl.value = ""
+  })
+  .catch(function(err) {
+    console.log(err)
+  })
 }
 
 function renderWeather(weatherObj) {
@@ -40,19 +40,19 @@ function renderWeather(weatherObj) {
 
   // render weather icon
   var weathericon = document.createElement('img')
-  weathericon.src = weatherObj.icon
-  weathericon.alt = weatherObj.description
+  weathericon.src = weatherObj.weather.icon
+  weathericon.alt = weatherObj.weather.description
   weatherEl.appendChild(weathericon)
 
 
   // render current temperature
   var temperature = document.createElement('p')
-  temperature.textContent = weatherObj.temp
+  temperature.textContent = 'Current ' + weatherObj.main.temp
   weatherEl.appendChild(temperature)
 
   // render feels like temperature
   var feels = document.createElement('p')
   feels.style.fontStyle = 'italic'
-  feels.textContent = weatherObj.feels_like
+  feels.textContent = 'Feels like ' + weatherObj.main.feels_like
   weatherEl.appendChild(feels)
 }
