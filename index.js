868917f51478f7083b34c0f14ed44258a884a535
Carrie Smith <smithcarrierenee@gmail.com>
2021-10-30 14:11:06 -0400

    updated js file

--- index.js
+++ index.js
@@ -11,49 +11,48 @@ formEl.onsubmit = function(e) {
     var weatherQuery = inputEl.value.trim()
     if (!weatherQuery) return
     fetch('https://api.openweathermap.org/data/2.5/weather?appid=9e38de1e0875f34d69ce021be2375874&q=' + weatherQuery)
-    .then(function(response) {
-      return response.json()
+    .then(function(res) {
+      return res.json()
     })
-    .then(function(result) {
-      console.log(result)
+    .then(function(res) {
+      console.log(res)
       renderWeather(res)
       inputEl.value = ""
     })
     .catch(function(err) {
       console.log(err)
     })
-  }
+}
 
 function renderWeather(weatherObj) {
   // clear previous weather
   weatherEl.innerHTML = ""
   // handle weaather not found
   if (weatherObj.Response === 'False') {
-    weatherEl.textContent = 'Movie not found'
+    weatherEl.textContent = 'Weather not found'
     return
   }
 
- // render current weather
- var currentweatherEl = document.createElement('h2')
- currentweatherEl.textContent = weatherObj.weather
- weatherEl.appendChild(currenweatherEl)
+  // render current weather
+  var currentweatherEl = document.createElement('h2')
+  currentweatherEl.textContent = weatherObj.weather
+  weatherEl.appendChild(currentweatherEl)
 
- // render weather icon
- var weathericon = document.createElement('img')
- weathericon.src = weatherObj.icon
- weathericon.alt = weatherObj.description
- weatherEl.appendChild(weathericon)
-
-
- // render current temperature
- var temperature = document.createElement('p')
- temperature.textContent = weatherObj.temp
- weatherEl.appendChild(temperature)
-
- // render feels like temperature
- var feels = document.createElement('p')
- feels.style.fontStyle = 'italic'
- feels.textContent = weatherObj.feels_like
- weatherEl.appendChild(feels)
+  // render weather icon
+  var weathericon = document.createElement('img')
+  weathericon.src = weatherObj.icon
+  weathericon.alt = weatherObj.description
+  weatherEl.appendChild(weathericon)
 
 
+  // render current temperature
+  var temperature = document.createElement('p')
+  temperature.textContent = weatherObj.temp
+  weatherEl.appendChild(temperature)
+
+  // render feels like temperature
+  var feels = document.createElement('p')
+  feels.style.fontStyle = 'italic'
+  feels.textContent = weatherObj.feels_like
+  weatherEl.appendChild(feels)
+}
